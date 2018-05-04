1f6b04cbb7cb3ec47a002bb3838eaaa57fea661e
awongh <akira@awongh.com>
2018-05-04 18:45:19 +0800

    taking out stuff

--- src/client/App.js
+++ src/client/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import {hot} from 'react-hot-loader';
-import {Provider} from 'react-redux';
-import {IntlProvider} from 'react-intl';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-import store from './store';
-import Router from './router';
-import muiTheme from './muiTheme';
 
 class App extends React.Component {
   render() {
     return (
-      <Provider store={store}>
-        <IntlProvider locale="en">
-          <MuiThemeProvider muiTheme={muiTheme}>
-            <Router />
-          </MuiThemeProvider>
-        </IntlProvider>
-      </Provider>
+      <div>
+        Hello World
+      </div>
     );
   }
 }
