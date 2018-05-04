36faee9540b920b997601d86d308a1cda90f735f
awongh <akira@awongh.com>
2018-05-04 18:25:06 +0800

    changes

--- src/server/index.js
+++ src/server/index.js
@@ -7,19 +7,6 @@ const logger = require('./logger');
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 process.env.HTTP_PORT = process.env.HTTP_PORT || 3000;
 
-function onUnhandledError(err) {
-  try {
-    logger.error(err);
-  } catch (e) {
-    console.log('LOGGER ERROR:', e); //eslint-disable-line no-console
-    console.log('APPLICATION ERROR:', err); //eslint-disable-line no-console
-  }
-  process.exit(1);
-}
-
-process.on('unhandledRejection', onUnhandledError);
-process.on('uncaughtException', onUnhandledError);
-
 const setupAppRoutes =
   process.env.NODE_ENV === 'development' ? require('./middlewares/development') : require('./middlewares/production');
 
