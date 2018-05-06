0075bf775bdad6219d6535c0a0232f674d5d7115
awongh <akira@awongh.com>
2018-05-06 18:22:58 +0800

    take out fetch from other branch

--- src/client/components/counter/counter.jsx
+++ src/client/components/counter/counter.jsx
@@ -8,21 +8,11 @@ class Counter extends React.Component {
   constructor() {
     super();
     this.state = {
-      pokemon: [],
       banana: 'sneakers'
     };
   }
 
-  componentDidMount() {
-    fetch('/api/pokemon/1')
-      .then((response) => response.json())
-      .then((myJson) => {
-        this.setState({pokemon: [myJson, ...this.state.pokemon]});
-      });
-  }
-
   render() {
-    console.log(this.state.pokemon);
     return (
       <p className={styles.desc}>
         {this.props.message} : {this.state.banana}
